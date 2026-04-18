6e4662dbfca549d142ff5af97e57affb15af5648
Abhiram M D <abmd4694@gmail.com>
2026-04-18 21:16:51 +0530

    Increase JSON and URL-encoded request size limits

--- analyst-pro-backend/index.js
+++ analyst-pro-backend/index.js
@@ -27,7 +27,8 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }))
-
+app.use(express.json({ limit: '50mb' }))
+app.use(express.urlencoded({ extended: true, limit: '50mb' }))
 
 // Static uploads directory
 app.use('/uploads', express.static(join(__dirname, 'uploads')))
